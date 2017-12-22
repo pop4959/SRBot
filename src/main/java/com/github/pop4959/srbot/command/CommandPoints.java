cdc449c96041e78440f72eae6e8426f206e93e30
chbohn <cbohn@uci.edu>
2017-12-22 11:32:03 +0100

    Update points command for Winter season

--- src/main/java/com/github/pop4959/srbot/command/CommandPoints.java
+++ src/main/java/com/github/pop4959/srbot/command/CommandPoints.java
@@ -14,13 +14,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class CommandPoints extends BotCommand {
 
-    private static final Pair<String, Integer>[] BOUNDARIES = new Pair[]{Pair.of("Entry", 0), Pair.of("Beginner", 800), Pair.of("Advanced", 900), Pair.of("Expert", 1000), Pair.of("Bronze", 1200), Pair.of("Silver", 1700), Pair.of("Gold", 2000), Pair.of("Platinum", 2400), Pair.of("Diamond", 2900)};
+    private static final Pair<String, Integer>[] BOUNDARIES = new Pair[]{Pair.of("Entry", 0), Pair.of("Beginner", 8000), Pair.of("Advanced", 9000), Pair.of("Expert", 10000), Pair.of("Bronze", 12000), Pair.of("Silver", 17000), Pair.of("Gold", 20000), Pair.of("Platinum", 24000), Pair.of("Diamond", 29000)};
 
     public CommandPoints() {
         super("points");
@@ -34,18 +35,18 @@ public class CommandPoints extends BotCommand {
                 event.getChannel().sendMessage("Invalid Steam ID or vanity URL provided.").queue();
                 return;
             }
-            String jsonString = "";
+            StringBuilder jsonStringBuilder = new StringBuilder();
             try {
                 URL url = new URL("http://api.speedrunners.doubledutchgames.com/GetRanking?id=" + id);
                 BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
                 int character;
-                while ((character = in.read()) > -1) {
-                    jsonString += ((char) character);
-                }
+                while ((character = in.read()) > -1)
+                    jsonStringBuilder.append((char) character);
             } catch (IOException e) {
                 event.getChannel().sendMessage("The requested user does not own the game, is unranked, or has their profile set to private.").queue();
                 return;
             }
+            String jsonString = jsonStringBuilder.toString();
             if (!jsonString.contains("{")) {
                 event.getChannel().sendMessage("The requested user does not own the game, is unranked, or has their profile set to private.").queue();
                 return;
@@ -56,10 +57,9 @@ public class CommandPoints extends BotCommand {
                 eloTier++;
             }
             try {
-                event.getChannel().sendMessage(EmbedTemplates.plaintext("Points for " + user.getPlayerProfile(Long.parseLong(id)).get(Integer.parseInt(Data.fromJSON("queryTimeout")), TimeUnit.MILLISECONDS).getName(), "Beta season: " + jsonData.getDouble("rating") + " points [" + BOUNDARIES[eloTier].getLeft() + " League]\nOff-season: " + jsonData.getInt("score") + " points [" + BOUNDARIES[jsonData.getInt("tier")].getLeft() + " League]").build()).queue();
+                event.getChannel().sendMessage(EmbedTemplates.plaintext("Points for " + user.getPlayerProfile(Long.parseLong(id)).get(Integer.parseInt(Data.fromJSON("queryTimeout")), TimeUnit.MILLISECONDS).getName(), "Winter season: " + (new DecimalFormat("#.##")).format(jsonData.getDouble("rating")) + " points [" + BOUNDARIES[eloTier].getLeft() + " League]\nOff-season: " + jsonData.getInt("score") + " points [" + BOUNDARIES[jsonData.getInt("tier")].getLeft() + " League]").build()).queue();
             } catch (BadRequestException | InterruptedException | ExecutionException | TimeoutException e) {
                 event.getChannel().sendMessage("Unable to retrieve data for the requested user.").queue();
-                return;
             }
         } else {
             event.getChannel().sendMessage("Please provide a Steam ID or vanity URL.").queue();

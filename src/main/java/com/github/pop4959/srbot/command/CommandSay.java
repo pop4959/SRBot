1e9040ccdbc6e92f71b8db99ddeb8f78c8d7a550
pop4959 <pop4959@gmail.com>
2018-06-22 22:56:46 -0700

    add say command

---
+++ src/main/java/com/github/pop4959/srbot/command/CommandSay.java
@@ -0,0 +1,16 @@
+package com.github.pop4959.srbot.command;
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import org.apache.commons.lang3.StringUtils;
+
+public class CommandSay extends BotCommand {
+
+    public CommandSay() {
+        super("say");
+    }
+
+    public void execute(MessageReceivedEvent event, String[] args) {
+        event.getChannel().sendMessage(StringUtils.join(args, " ")).queue();
+    }
+
+}

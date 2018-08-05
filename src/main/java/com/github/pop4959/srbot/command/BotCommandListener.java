d7c24ca82c2a1fbee0c801296f428091029140f0
pop4959 <pop4959@gmail.com>
2018-08-04 23:23:19 -0700

    fix commands in private messages

--- src/main/java/com/github/pop4959/srbot/command/BotCommandListener.java
+++ src/main/java/com/github/pop4959/srbot/command/BotCommandListener.java
@@ -2,11 +2,11 @@ package com.github.pop4959.srbot.command;
 
 import com.github.pop4959.srbot.data.Data;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.apache.commons.lang3.StringUtils;
-
-import java.util.List;
 
 public class BotCommandListener extends ListenerAdapter {
 
@@ -20,11 +20,12 @@ public class BotCommandListener extends ListenerAdapter {
             if (BotCommandHandler.isCommand(commandString)) {
                 String[] args = StringUtils.substringAfter(message, " ").split(" ");
                 BotCommand command = BotCommandHandler.getCommand(commandString);
-                if (command.getConfig().isExclusive() && event.getGuild().getIdLong() != SERVER) {
+                Guild guild = event.getGuild();
+                if (command.getConfig().isExclusive() && (guild == null || guild.getIdLong() != SERVER)) {
                     event.getChannel().sendMessage("That command cannot be used on this server.").queue();
                     return;
                 }
-                if (command.getConfig().getPermission().equalsIgnoreCase("default") || event.getMember().hasPermission(Permission.MANAGE_SERVER)) {
+                if (command.getConfig().getPermission().equalsIgnoreCase("default") || event.getChannel().getType() == ChannelType.PRIVATE || event.getMember().hasPermission(Permission.MANAGE_SERVER)) {
                     command.execute(event, args.length == 1 && args[0] == "" ? new String[]{} : args);
                 }
             }
--- src/main/java/com/github/pop4959/srbot/command/CommandCommands.java
+++ src/main/java/com/github/pop4959/srbot/command/CommandCommands.java
@@ -3,6 +3,7 @@ package com.github.pop4959.srbot.command;
 import com.github.pop4959.srbot.data.CommandsData;
 import com.github.pop4959.srbot.data.Data;
 import com.github.pop4959.srbot.util.EmbedTemplates;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 import java.util.ArrayList;
@@ -17,10 +18,11 @@ public class CommandCommands extends BotCommand {
     }
 
     public void execute(MessageReceivedEvent event, String[] args) {
+        Guild guild = event.getGuild();
         List<String> commands = new ArrayList<>();
         for (String cmdName : Data.commands().getCommandConfigurationMap().keySet()) {
             CommandsData.CommandConfiguration cc = BotCommandHandler.getCommand(cmdName).getConfig();
-            if (!cc.isHidden() && (!cc.isExclusive() || event.getGuild().getIdLong() == SERVER))
+            if (!cc.isHidden() && (!cc.isExclusive() || (guild != null && guild.getIdLong() == SERVER)))
                 commands.add(cmdName);
         }
         event.getChannel().sendMessage(EmbedTemplates.plaintext(event.getGuild(), "Commands", commands.toString().replaceAll("[\\[\\]]", "")).build()).queue();

80a56037e3aa6ea2acaa334a9c1a42a531b7ee03
pop4959 <pop4959@gmail.com>
2018-12-22 00:14:19 -0800

    Add seasons to points command

--- src/main/java/com/github/pop4959/srbot/command/CommandPoints.java
+++ src/main/java/com/github/pop4959/srbot/command/CommandPoints.java
@@ -9,17 +9,21 @@ import com.ibasco.agql.protocols.valve.steam.webapi.interfaces.SteamUser;
 import com.ibasco.agql.protocols.valve.steam.webapi.pojos.SteamPlayerProfile;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.apache.commons.lang3.tuple.Pair;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 public class CommandPoints extends BotCommand {
 
@@ -32,44 +36,110 @@ public class CommandPoints extends BotCommand {
     }
 
     public void execute(MessageReceivedEvent event, String[] args) {
-        if (args.length > 0) {
-            SteamUser user = new SteamUser(Main.getClient());
-            String id = Steam.resolveID64(user, args[0]);
-            if (id == null) {
-                event.getChannel().sendMessage("Invalid Steam ID or vanity URL provided.").queue();
-                return;
-            }
-            StringBuilder jsonStringBuilder = new StringBuilder();
-            try {
-                URL url = new URL("http://api.speedrunners.doubledutchgames.com/GetRanking?id=" + id);
-                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-                int character;
-                while ((character = in.read()) > -1)
-                    jsonStringBuilder.append((char) character);
-            } catch (IOException e) {
-                event.getChannel().sendMessage("The requested user does not own the game, is unranked, or has their profile set to private.").queue();
-                return;
-            }
-            String jsonString = jsonStringBuilder.toString();
-            if (!jsonString.contains("{")) {
-                event.getChannel().sendMessage("The requested user does not own the game, is unranked, or has their profile set to private.").queue();
-                return;
-            }
-            JSONObject jsonData = new JSONObject(jsonString);
-            int eloTier = 0;
-            while (eloTier < 8 && BOUNDARIES[eloTier + 1].getRight() <= jsonData.getDouble("rating")) {
-                eloTier++;
-            }
-            boolean isCurrentSeason = jsonData.getInt("season") == 4;
-            try {
-                SteamPlayerProfile steamProfile = user.getPlayerProfile(Long.parseLong(id)).get(Data.config().getQueryTimeout(), TimeUnit.MILLISECONDS);
-                event.getChannel().sendMessage(EmbedTemplates.points(event.getGuild(), "Off-season " + RANK_EMOTES.get(id.equals(KOS) ? 9 : jsonData.getInt("tier")) + " " + BOUNDARIES[jsonData.getInt("tier")].getLeft() + " League (" + jsonData.getInt("score") + " points)" + (isCurrentSeason ? ("\nChristmas season " + RANK_EMOTES.get(id.equals(KOS) ? 9 : eloTier) + " " + BOUNDARIES[eloTier].getLeft() + " League (" + (new DecimalFormat("#.##")).format(jsonData.getDouble("rating")) + " points)") : ""), "Points for " + steamProfile.getName(), steamProfile.getProfileUrl(), steamProfile.getAvatarFullUrl()).build()).queue();
-            } catch (BadRequestException | InterruptedException | ExecutionException | TimeoutException e) {
-                event.getChannel().sendMessage("Unable to retrieve data for the requested user.").queue();
-            }
-        } else {
+        if (args.length == 0) {
             event.getChannel().sendMessage("Please provide a Steam ID or vanity URL.").queue();
+            return;
         }
+        SteamUser user = new SteamUser(Main.getClient());
+        String id = Steam.resolveID64(user, args[0]);
+        if (id == null) {
+            event.getChannel().sendMessage("Invalid Steam ID or vanity URL provided.").queue();
+            return;
+        }
+        URL currentSeasonUrl, oldSeasonUrl;
+        try {
+            currentSeasonUrl = new URL("http://api.speedrunners.doubledutchgames.com/GetRanking?id=" + id);
+            oldSeasonUrl = new URL("http://api.speedrunners.doubledutchgames.com/GetSeasonHistory?id=" + id);
+        } catch (MalformedURLException e) {
+            event.getChannel().sendMessage("The requested user does not own the game, is unranked, or has their profile set to private.").queue();
+            return;
+        }
+        URLConnection currentSeasonConn, oldSeasonConn;
+        try {
+            currentSeasonConn = currentSeasonUrl.openConnection();
+            oldSeasonConn = oldSeasonUrl.openConnection();
+            if (currentSeasonConn == null || oldSeasonConn == null) throw new NullPointerException();
+        } catch (Exception e) {
+            event.getChannel().sendMessage("The requested user does not own the game, is unranked, or has their profile set to private.").queue();
+            return;
+        }
+        String currentSeasonJson, oldSeasonJson;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(currentSeasonConn.getInputStream(), StandardCharsets.UTF_8))) {
+            currentSeasonJson = reader.lines().collect(Collectors.joining("\n"));
+            if (currentSeasonJson == null) throw new NullPointerException();
+            if (!currentSeasonJson.contains("{")) throw new IllegalArgumentException();
+        } catch (Exception e) {
+            event.getChannel().sendMessage("The requested user does not own the game, is unranked, or has their profile set to private.").queue();
+            return;
+        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(oldSeasonConn.getInputStream(), StandardCharsets.UTF_8))) {
+            oldSeasonJson = reader.lines().collect(Collectors.joining("\n"));
+            if (oldSeasonJson == null) throw new NullPointerException();
+            if (!oldSeasonJson.contains("{")) throw new IllegalArgumentException();
+        } catch (Exception e) {
+            event.getChannel().sendMessage("The requested user does not own the game, is unranked, or has their profile set to private.").queue();
+            return;
+        }
+        SteamPlayerProfile steamProfile = null;
+        try {
+            steamProfile = user.getPlayerProfile(Long.parseLong(id)).get(Data.config().getQueryTimeout(), TimeUnit.MILLISECONDS);
+            if (steamProfile == null) throw new NullPointerException();
+        } catch (BadRequestException | InterruptedException | ExecutionException | TimeoutException | NullPointerException e) {
+            event.getChannel().sendMessage("Unable to retrieve data for the requested user.").queue();
+            return;
+        }
+        JSONObject currentSeasonData = new JSONObject(currentSeasonJson);
+        boolean isCurrentSeason = currentSeasonData.getInt("season") == 4;
+        int currentEloTier = 0;
+        while (currentEloTier < 8 && BOUNDARIES[currentEloTier + 1].getRight() <= currentSeasonData.getDouble("rating")) {
+            currentEloTier++;
+        }
+        StringBuilder embedMessage = new StringBuilder();
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        addSeason(embedMessage, "Off-Season", RANK_EMOTES.get(id.equals(KOS) ? 9 : currentSeasonData.getInt("tier")), BOUNDARIES[currentSeasonData.getInt("tier")].getLeft(), Integer.toString(currentSeasonData.getInt("score")));
+        JSONArray oldSeasonDataArray = new JSONArray(oldSeasonJson);
+        for (Object o : oldSeasonDataArray) {
+            try {
+                JSONObject oldSeasonData = (JSONObject) o;
+                try {
+                    switch (oldSeasonData.getInt("seasonid")) {
+                        case 2:
+                            int season2EloTier = 0;
+                            while (season2EloTier < 8 && (BOUNDARIES[season2EloTier + 1].getRight() / 10) <= oldSeasonData.getDouble("score")) {
+                                season2EloTier++;
+                            }
+                            addSeason(embedMessage, "Beta Season", RANK_EMOTES.get(id.equals(KOS) ? 9 : season2EloTier), BOUNDARIES[season2EloTier].getLeft(), decimalFormat.format(oldSeasonData.getDouble("score")));
+                            break;
+                        case 3:
+                            int season3EloTier = 0;
+                            while (season3EloTier < 8 && BOUNDARIES[season3EloTier + 1].getRight() <= oldSeasonData.getDouble("score")) {
+                                season3EloTier++;
+                            }
+                            addSeason(embedMessage, "Winter Season", RANK_EMOTES.get(id.equals(KOS) ? 9 : season3EloTier), BOUNDARIES[season3EloTier].getLeft(), decimalFormat.format(oldSeasonData.getDouble("score")));
+                            break;
+                        default:
+                            break;
+                    }
+                } catch (Exception e) {
+                }
+            } catch (ClassCastException e) {
+            }
+        }
+        if (isCurrentSeason) {
+            addSeason(embedMessage, "Christmas Season", RANK_EMOTES.get(id.equals(KOS) ? 9 : currentEloTier), BOUNDARIES[currentEloTier].getLeft(), decimalFormat.format(currentSeasonData.getDouble("rating")));
+        }
+        event.getChannel().sendMessage(EmbedTemplates.points(event.getGuild(), embedMessage.toString(), "Points for " + steamProfile.getName(), steamProfile.getProfileUrl(), steamProfile.getAvatarFullUrl()).build()).queue();
+    }
+
+    private static void addSeason(StringBuilder embedMessage, String season, String emote, String league, String points) {
+        embedMessage.append(season);
+        embedMessage.append(" ");
+        embedMessage.append(emote);
+        embedMessage.append(" ");
+        embedMessage.append(league);
+        embedMessage.append(" League (");
+        embedMessage.append(points);
+        embedMessage.append(" points)\n");
     }
 
 }

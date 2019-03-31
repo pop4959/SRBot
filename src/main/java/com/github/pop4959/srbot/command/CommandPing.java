542bd76b2b7be4f4f0c96180796b7fe28a26cb1c
S0rax <marcinqwertyuiop@gmail.com>
2019-03-31 19:43:49 +0200

    test

--- src/main/java/com/github/pop4959/srbot/command/CommandPing.java
+++ src/main/java/com/github/pop4959/srbot/command/CommandPing.java
@@ -10,6 +10,7 @@ public class CommandPing extends BotCommand {
 
     public void execute(MessageReceivedEvent event, String[] args) {
         event.getChannel().sendMessage("Pong! (" + event.getJDA().getPing() + "ms)").queue();
+        event.getChannel().sendMessage("XD").queue();
     }
 
 }
